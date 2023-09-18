6a7eb0d2b33e2c0968b0b10d4e64becb7a11f659
Bondka_dali <bondkadali02@gmail.com>
2023-09-18 19:05:10 +0100

    pushed final version of cloud configuration service

---
+++ src/test/java/com/example/springcloudconfiguration/SpringCloudConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
